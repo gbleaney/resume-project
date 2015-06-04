b0413dd81b87e1a44277032c67c548003f0da6eb
Graham Bleaney <gbleaney@gmail.com>
2015-06-03 21:58:41 -0400

    Added user authentication and add/remove points

--- resume-project.js
+++ resume-project.js
@@ -1,23 +1,70 @@
+Points = new Mongo.Collection("points");
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Meteor.subscribe("points");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    points: function() {
+      return Points.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  Template.body.events({
+  "submit .new-point": function (event) {
+    // This function is called when the new task form is submitted
+
+    var text = event.target.text.value;
+
+    Meteor.call("addPoint", text);
+
+    // Clear form
+    event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  },
+
+  "click .delete": function () {
+    Meteor.call("deletePoint", this._id);
+  }
+
+});
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.publish("points", function () {
+    return Points.find({owner: this.userId});
+  });
 }
+
+
+Meteor.methods({
+  addPoint: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Points.insert({
+      point: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deletePoint: function (pointId) {
+    var point = Points.findOne(pointId);
+    if (point.owner !== Meteor.userId()) {
+      // Make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Points.remove(pointId);
+  },
+});
